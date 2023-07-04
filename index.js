17e7fb2d724e74892497c688eb0d8a92ac625bc9
CelestialJerusha <jerushaamuri@gmail.com>
2023-07-05 01:26:23 +0530

    commit

--- index.js
+++ index.js
@@ -67,13 +67,13 @@ function removeItem(id){
 function searchData(){
 var input, filter,table, tr, td, i,txtValue;
 input = document.getElementById("searchBar");
+filter = input.value.toUpperCase();
 table = document.getElementById("table");
 tr = table.getElementsByTagName("tr");
 
-filter = input.value.toUpperCase();
 
 for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[1];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
